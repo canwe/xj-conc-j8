6150e55edc2439dcd1c0b86b39e26ca8e62bf62a
Victor Kanopelko <vrebesbyrd@gmail.com>
2019-05-09 12:30:02 +0300

    ex_11_1: walking collection

--- src/xj_conc/ch11_explicit_locks/exercise_11_1/WalkingCollection.java
+++ src/xj_conc/ch11_explicit_locks/exercise_11_1/WalkingCollection.java
@@ -1,66 +1,112 @@
 package xj_conc.ch11_explicit_locks.exercise_11_1;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * TODO: Fix the WalkingCollection to allow multiple threads to call the
- * TODO: iterate() method concurrently and to still be threadsafe
+ * WalkingCollection allows multiple threads to call the
+ * iterate() method concurrently and is still threadsafe
  */
-public class WalkingCollection<E>
-    extends AbstractCollection<E> {
+public class WalkingCollection<E> extends AbstractCollection<E> {
+
     private final Collection<E> wrappedCollection;
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public WalkingCollection(Collection<E> wrappedCollection) {
         this.wrappedCollection = wrappedCollection;
     }
 
-    public synchronized void iterate(Processor<? super E> processor) {
-        // TODO: lock using a ReadLock, then iterate through collection calling
-        // TODO: processor.process(e) on each element
-        for (E e : wrappedCollection) {
-            if (!processor.process(e)) return;
+    public void iterate(Processor<? super E> processor) {
+        // lock using a ReadLock, then iterate through collection calling
+        // processor.process(e) on each element
+
+        ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            for (E e : wrappedCollection) {
+                if (!processor.process(e)) return;
+            }
+        } finally {
+            readLock.unlock();
         }
     }
 
-    public synchronized Iterator<E> iterator() {
-        // TODO: this method should not really be called by users anymore, instead
-        // TODO: they should call the iterate(Processor) method
+    public Iterator<E> iterator() {
+        // this method should not really be called by users anymore, instead
+        // they should call the iterate(Processor) method
 
-        // TODO: return an iterator that locks a ReadLock on hasNext() and next()
-        // TODO: and a WriteLock on remove().
+        // return an iterator that locks a ReadLock on hasNext() and next()
+        // and a WriteLock on remove().
 
-        // TODO: Should throw IllegalMonitorStateException if a thread tries to
-        // TODO: call remove() during iteration.
-        Iterator<E> wrappedIterator = wrappedCollection.iterator();
-        return new Iterator<E>() {
-            public boolean hasNext() {
-                synchronized (WalkingCollection.this) {
-                    return wrappedIterator.hasNext();
+        // Should throw IllegalMonitorStateException if a thread tries to
+        // call remove() during iteration.
+        ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            Iterator<E> wrappedIterator = wrappedCollection.iterator();
+            return new Iterator<E>() {
+                public boolean hasNext() {
+                    ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+                    readLock.lock();
+                    try {
+                        return wrappedIterator.hasNext();
+                    } finally {
+                        readLock.unlock();
+                    }
                 }
-            }
 
-            public E next() {
-                synchronized (WalkingCollection.this) {
-                    return wrappedIterator.next();
+                public E next() {
+                    ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+                    readLock.lock();
+                    try {
+                        return wrappedIterator.next();
+                    } finally {
+                        readLock.unlock();
+                    }
                 }
-            }
 
-            public void remove() {
-                synchronized (WalkingCollection.this) {
-                    wrappedIterator.remove();
+                public void remove() {
+                    if (lock.getReadHoldCount() > 0) {
+                        throw new IllegalMonitorStateException("thread is already busy iterating");
+                    }
+                    ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+                    writeLock.lock();
+                    try {
+                        wrappedIterator.remove();
+                    } finally {
+                        writeLock.unlock();
+                    }
                 }
-            }
-        };
+            };
+        } finally {
+            readLock.unlock();
+        }
     }
 
-    public synchronized int size() {
-        // TODO: the size of the wrappedCollection, but wrapped with a ReadLock
-        return wrappedCollection.size();
+    public int size() {
+        // the size of the wrappedCollection, but wrapped with a ReadLock
+        ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            return wrappedCollection.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 
-    public synchronized boolean add(E e) {
-        // TODO: adds the element to the collection, throws
-        // TODO: IllegalMonitorStateException if that thread is busy iterating
-        return wrappedCollection.add(e);
+    public boolean add(E e) {
+        // adds the element to the collection, throws
+        // IllegalMonitorStateException if that thread is busy iterating
+        if (lock.getReadHoldCount() > 0) {
+            throw new IllegalMonitorStateException("thread is already busy iterating");
+        }
+        ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            return wrappedCollection.add(e);
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
